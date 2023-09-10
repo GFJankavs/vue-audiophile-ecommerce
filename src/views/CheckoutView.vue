eb44bc142c223e82cac1e221b76820fb5b535ebf
Gustavs Fricis Jankavs <gfjankavs@tutamail.com>
2023-09-10 19:39:11 +0300

    feat(app): Audiophile - Checkout View
    Initial work on checkout view for Audiophile Ecommerce website

--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -1,5 +1,58 @@
 <template>
-  <div>This is checkout view</div>
+  <div class="container">
+    <button class="checkout__back font__body" @click="onGoBack">Go Back</button>
+    <div class="checkout__grid">
+      <div class="wrapper">
+        <div class="checkout__info">
+          <h4 class="font__h4">Checkout</h4>
+          <div class="checkout__info-section">
+            <span class="font__subtitle checkout__title">billing details</span>
+          </div>
+        </div>
+      </div>
+      <div>Summary</div>
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import router from '@/router'
+
+const onGoBack = () => {
+  router.back()
+}
+</script>
+
+<style scoped>
+.checkout__back {
+  all: unset;
+  cursor: pointer;
+  margin: 16px 0 24px;
+}
+
+.checkout__grid {
+  display: grid;
+  gap: 32px;
+  margin-bottom: 96px;
+}
+
+.wrapper {
+  background: #fff;
+  border-radius: 8px;
+}
+
+.checkout__info {
+  display: grid;
+  gap: 32px;
+}
+
+.checkout__info-section {
+  display: grid;
+  gap: 16px;
+}
+
+.checkout__inputs {
+  display: grid;
+  gap: 24px;
+}
+</style>
